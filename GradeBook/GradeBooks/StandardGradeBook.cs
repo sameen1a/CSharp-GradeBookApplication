81cbdeb09d4f65218ae0e10d2a39517d596b36af
Sameen Ahmad <sameen.ahmad@s00967-mbpr.2d.hq>
2018-08-30 10:26:25 -0700

    Creating New GradeBookTypes

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,11 @@
+using GradeBook.Enums;
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type GradeBookType.Standard;
+        }
+    }
+}
